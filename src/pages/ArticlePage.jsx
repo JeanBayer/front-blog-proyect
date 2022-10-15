a1c64bf3b8d5494f7b4717ffe49dc8fb336cf0d2
Jhan Bayer <jhanbayer@gmail.com>
2022-10-15 13:42:04 -0300

    feat(article): Display article by name url

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlePage = () => {
-  return <div>ArticlePage</div>;
+  const { articleId } = useParams();
+  const article = articles.find((article) => article.name === articleId);
+
+  return (
+    <>
+      <h1> {article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+    </>
+  );
 };
 
 export default ArticlePage;
