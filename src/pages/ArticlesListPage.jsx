f625c313bab191bedc8a7fa938a2cc72e12b82d3
Jhan Bayer <jhanbayer@gmail.com>
2022-10-15 14:58:56 -0300

    refactor(articles): Create a Article List component for reusability

--- src/pages/ArticlesListPage.jsx
+++ src/pages/ArticlesListPage.jsx
@@ -1,20 +1,11 @@
-import { Link } from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
 import articles from "./article-content";
 
 const ArticlesListPage = () => {
   return (
     <>
       <h1>Articles</h1>
-      {articles.map((article) => (
-        <Link
-          key={article.name}
-          to={`/articles/${article.name}`}
-          className="article-list-item"
-        >
-          <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0, 150)}...</p>
-        </Link>
-      ))}
+      <ArticlesList articles={articles} />
     </>
   );
 };
