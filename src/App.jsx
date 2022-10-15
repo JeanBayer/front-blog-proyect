035a77929dfd950bfd617f1181664eef0a51670c
Jhan Bayer <jhanbayer@gmail.com>
2022-10-15 15:16:21 -0300

    feat(not found): Add not found page and lorem  text

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AboutPage from "./pages/AboutPage";
 import ArticlePage from "./pages/ArticlePage";
 import ArticlesListPage from "./pages/ArticlesListPage";
 import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <Route path="/about" element={<AboutPage />} />
               <Route path="/articles" element={<ArticlesListPage />} />
               <Route path="/articles/:articleId" element={<ArticlePage />} />
+              <Route path="*" element={<NotFoundPage />} />
             </Routes>
           </div>
         </div>
