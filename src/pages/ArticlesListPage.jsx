1676158123d0ecf65664977a1f5808357f257df9
Jhan Bayer <jhanbayer@gmail.com>
2022-10-15 14:02:48 -0300

    feat(articles): Display all articles and linked

--- src/pages/ArticlesListPage.jsx
+++ src/pages/ArticlesListPage.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+import articles from "./article-content";
+
 const ArticlesListPage = () => {
-  return <div>ArticlesListPage</div>;
+  return (
+    <>
+      <h1>Articles</h1>
+      {articles.map((article) => (
+        <Link
+          key={article.name}
+          to={`/articles/${article.name}`}
+          className="article-list-item"
+        >
+          <h3>{article.title}</h3>
+          <p>{article.content[0].substring(0, 150)}...</p>
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default ArticlesListPage;
